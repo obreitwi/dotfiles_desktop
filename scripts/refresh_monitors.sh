83dfb1bd7522f7ad3a5e378dd688b43746a60a52
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2018-07-24 15:04:09 +0200

    Fix monitor settings in xinitrc

--- scripts/refresh_monitors.sh
+++ scripts/refresh_monitors.sh
@@ -1,31 +1,8 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
-# does not work reliably yet! --obreitwi, 04-10-17 20:49:19
+source $HOME/.zsh/rc_functions
+source $HOME/.zsh/hosts/$(mdh)
 
-setopt extended_glob
-setopt glob_subst
+monitors_default
 
-watchpath="/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-*/status"
-
-while true; do
-inotifywatch -e modify ${watchpath}
-echo "Refreshing mintors.."
-case "$(hostname)" in
-abed)
-if xrandr | grep -q "HDMI2 connected"; then
-    # "setup @ home"
-    xrandr --output eDP1 --auto --output HDMI2 --primary --auto --right-of eDP1
-elif xrandr | grep -q "DP2-2 connected"; then
-    # "setup @ office"
-    xrandr --output eDP1 --auto --output DP2-1 --primary --auto --right-of eDP1 \
-           --output DP2-2 --auto --right-of DP2-1
-else
-    # "setup @ mobile"
-    xrandr --output eDP1 --primary --auto
-fi
-
-;;
-esac
-sleep 1
-done
 
