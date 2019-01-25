0a4eaecd4d4725cafe9ba6a177db4fc5665fd083
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-01-25 12:36:41 +0100

    Track all xmonad setup files

--- xmonad/install.sh
+++ xmonad/install.sh
@@ -5,9 +5,11 @@ TGTFLD="$HOME/.xmonad"
 
 mkdir -v $TGTFLD
 
-ln -s -f -v $SRCFLD/xmonad.hs $TGTFLD/
-ln -s -f -v $SRCFLD/lib $TGTFLD/
-ln -s -f -v $SRCFLD/build $TGTFLD/
+ln -sfv $SRCFLD/xmonad.hs $TGTFLD/
+ln -sfv $SRCFLD/lib $TGTFLD/
+ln -sfv $SRCFLD/build $TGTFLD/
+ln -sfv $SRCFLD/stack.yaml $TGTFLD/
+ln -sfv $SRCFLD/mrconfig $TGTFLD/.mrconfig
 
 pydemx $SRCFLD/xmobar.pydemx
 
