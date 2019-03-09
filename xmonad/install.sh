3a4ed50114e4d733e8c242f080c8d95195912130
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-03-09 16:34:29 +0100

    Symlink ./xmonad to ~/.xmonad
    Allows code completion engines to find code from xmonad{,-contrib} more
    easily when modifying xmoand.hs

--- xmonad/install.sh
+++ xmonad/install.sh
@@ -3,13 +3,7 @@
 SRCFLD=$(dirname $(readlink -m "$0"))
 TGTFLD="$HOME/.xmonad"
 
-mkdir -v $TGTFLD
-
-ln -sfv $SRCFLD/xmonad.hs $TGTFLD/
-ln -sfv $SRCFLD/lib $TGTFLD/
-ln -sfv $SRCFLD/build $TGTFLD/
-ln -sfv $SRCFLD/stack.yaml $TGTFLD/
-ln -sfv $SRCFLD/mrconfig $TGTFLD/.mrconfig
+ln -sfv "${SRCFLD}" ${TGTFLD}
 
 pydemx $SRCFLD/xmobar.pydemx
 
