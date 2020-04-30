25d0695e0a3d439d0f97cdf7d434bce360b3195c
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-04-30 10:20:31 +0200

    Add udev rules to set backlight compensation for webcam

--- udev/install.sh
+++ udev/install.sh
@@ -7,4 +7,5 @@ sourcedir="$(dirname "$(readlink -m "${BASH_SOURCE[0]}")")"
 install -v -m 555 "${sourcedir}/restore_keyboard_settings_user.sh" "${HOME}/.local/bin"
 
 sudo install -v -m 444 "${sourcedir}/10-detect-daskeyboard.rules" /etc/udev/rules.d
+sudo install -v -m 444 "${sourcedir}/99-logitech-default-zoom.rules" /etc/udev/rules.d
 sudo install -v -m 555 ${sourcedir}/restore_keyboard_settings.sh /usr/local/bin
