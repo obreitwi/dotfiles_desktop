8d5c5b8f8953581cc80ce7192a77af3981d31da6
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-10-30 09:40:36 +0100

    Set neovim in urxvt as default text application

---
+++ applications/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SRCFLD=$(dirname "$(readlink -m "$0")")
+TGTFLD="${HOME}/.local/share/applications"
+
+[ ! -d "${TGTFLD}" ] && mkdir -vp "${TGTFLD}"
+
+ln -s -f -v "${SRCFLD}/nvim-urxvt.desktop" "${TGTFLD}"
+
+export XDG_UTILS_DEBUG_LEVEL=10
+
+for ft in $(grep MimeType "${SRCFLD}/nvim-urxvt.desktop" | cut -d= -f2 | tr ';' ' '); do
+    xdg-mime default nvim-urxvt.desktop "$ft";
+done
