5471a1f9e3c44eeed8226f6ff7009c24ebf83214
Oliver Breitwieser <oliver@breitwieser.eu>
2021-08-01 00:54:02 +0200

    Make zoom toggle more robust

--- zoom-toggle-audio/zoom-toggle-audio.sh
+++ zoom-toggle-audio/zoom-toggle-audio.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Adapted from: http://pzel.name/til/2019/11/25/Muting-and-unmuting-Zoom-from-anywhere-on-the-linux-desktop.html
 
 if ! which xdotool &>/dev/null; then
@@ -9,5 +9,6 @@ window_active="$(xdotool getwindowfocus)"
 window_zoom="$(xdotool search --limit 1 --name "Zoom Meeting")"
 xdotool windowactivate --sync "${window_zoom}"
 sleep 0.05
-xdotool key --clearmodifiers "alt+a"
+xdotool key --clearmodifiers --window "${window_zoom}" "alt+a"
+sleep 0.05
 xdotool windowactivate --sync "${window_active}"
