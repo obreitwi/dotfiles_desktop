f4d82c7dcd0328c6dbdddeebf4ec6b242255069d
Oliver Breitwieser <oliver@breitwieser.eu>
2021-09-04 14:41:58 +0200

    Rename restore keyboard scripts

--- udev/install.sh
+++ udev/install.sh
@@ -4,8 +4,8 @@ sudo -v
 
 sourcedir="$(dirname "$(readlink -m "${BASH_SOURCE[0]}")")"
 
-install -v -m 555 "${sourcedir}/restore_keyboard_settings_user.sh" "${HOME}/.local/bin"
+install -v -m 555 "${sourcedir}/daemon_restore_keyboard_settings_as_user.sh" "${HOME}/.local/bin/daemon_restore_keyboard_settings_as_user"
 
 sudo install -v -m 444 "${sourcedir}/10-detect-daskeyboard.rules" /etc/udev/rules.d
 sudo install -v -m 444 "${sourcedir}/99-logitech-default-zoom.rules" /etc/udev/rules.d
-sudo install -v -m 555 ${sourcedir}/restore_keyboard_settings.sh /usr/local/bin
+sudo install -v -m 555 ${sourcedir}/restore_keyboard_settings.sh /usr/local/bin/restore_keyboard_settings
