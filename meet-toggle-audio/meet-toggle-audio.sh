10a1c941fe9a621f93eab663400f07ac878d7c4b
Oliver Breitwieser <oliver@breitwieser.eu>
2021-09-09 14:04:01 +0200

    meet-toggle-audio: Explicitly send keyup for meta

--- meet-toggle-audio/meet-toggle-audio.sh
+++ meet-toggle-audio/meet-toggle-audio.sh
@@ -3,6 +3,8 @@ if ! which xdotool &>/dev/null; then
     notify-send "Did not find xdotool, cannot toggle Meet audio."
 fi
 
+xdotool keyup meta
+
 window_active="$(xdotool getwindowfocus)"
 sleep 0.05
 xdotool search --classname crx_kjgfgldnnfoeklkmfkjfagphfepbbdan windowactivate --sync key --clearmodifiers ctrl+d
