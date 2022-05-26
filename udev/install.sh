8ea1aacadbb01ae61cad7fa89d711918fe635ebc
Oliver Breitwieser <oliver@breitwieser.eu>
2022-05-26 21:23:03 +0200

    Add udev rule to connect to wifi once available

--- udev/install.sh
+++ udev/install.sh
@@ -7,5 +7,6 @@ sourcedir="$(dirname "$(readlink -m "${BASH_SOURCE[0]}")")"
 install -v -m 555 "${sourcedir}/daemon_restore_keyboard_settings_as_user.sh" "${HOME}/.local/bin/daemon_restore_keyboard_settings_as_user"
 
 sudo install -v -m 444 "${sourcedir}/10-detect-daskeyboard.rules" /etc/udev/rules.d
+sudo install -v -m 444 "${sourcedir}/20-connect-wifi.rules" /etc/udev/rules.d
 sudo install -v -m 444 "${sourcedir}/99-logitech-default-zoom.rules" /etc/udev/rules.d
-sudo install -v -m 555 ${sourcedir}/restore_keyboard_settings.sh /usr/local/bin/restore_keyboard_settings
+sudo install -v -m 555 "${sourcedir}/restore_keyboard_settings.sh" /usr/local/bin/restore_keyboard_settings
