98d4d8ea139a9acf8b4f6e5bd2f53b9c084e59cb
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-03-23 12:18:16 +0100

    fix: install.sh adapt to new xinitrc

--- x11/install.sh
+++ x11/install.sh
@@ -8,5 +8,6 @@ mkdir -p "${TGTFLD}"
 # disabled since not used right now
 ln -s -f -v "$SRCFLD/daemon_html_to_markdown_link.sh" "$TGTFLD/daemon_html_to_markdown_link"
 ln -s -f -v "$SRCFLD/client_html_to_markdown_link.sh" "$TGTFLD/html_to_markdown_link"
+ln -s -f -v "$SRCFLD/xinitrc" "$HOME/.xinitrc"
 # ln -s -f -v "$SRCFLD/brightness_down.sh"  "$TGTFLD"
 # ln -s -f -v "$SRCFLD/brightness_up.sh"  "$TGTFLD"
